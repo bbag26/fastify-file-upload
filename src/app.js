c376df801e242085683af465554d70ce558f4fa9
Gabriel M. dos Santos <bbag26@proton.me>
2023-08-09 15:32:42 -0500

    :recycle: refactor: reduce app.js file lines

--- src/app.js
+++ src/app.js
@@ -1,23 +1,17 @@
-const fastify = require('fastify')
-const cors = require('@fastify/cors')
-const multipart = require('@fastify/multipart')
-const static = require('@fastify/static')
+const fastify = require('fastify')({ logger: true })
 const { join } = require('path')
 
 const dotenv = require('dotenv')
-
 dotenv.config()
 
-const app = fastify({ logger: true })
-
-app.register(cors)
-app.register(multipart)
-app.register(static, {
+fastify.register(require('@fastify/cors'))
+fastify.register(require('@fastify/multipart'))
+fastify.register(require('@fastify/static'), {
   root: join(__dirname, '..', 'storage'),
   prefix: '/storage/'
 })
-app.register(require('./controllers/UploadController'))
+fastify.register(require('./controllers/UploadController'))
 
-app.listen({ port: process.env.APP_PORT }, () =>
+fastify.listen({ port: process.env.APP_PORT }, () =>
   console.log(`server is running in http://localhost:${process.env.APP_PORT}`)
 )
